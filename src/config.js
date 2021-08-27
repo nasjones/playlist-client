3293016372c4e8e48e499ef063ded91cf4c0a078
Nassir Jones <nasjonez1@gmail.com>
2021-08-27 12:53:11 -0400

    Revert "temporary commit"
    This reverts commit 2411af0b6140f8dfe62bf5ab0667900078b71976.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 module.exports = {
-    ENDPOINT: "http://localhost:8000/api",
-    // ENDPOINT: "https://fierce-plains-16077.herokuapp.com/api",
+    ENDPOINT: "https://fierce-plains-16077.herokuapp.com/api",
     REACT_APP_API_KEY: process.env.REACT_APP_API_KEY
 }
