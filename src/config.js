2411af0b6140f8dfe62bf5ab0667900078b71976
Nassir Jones <nasjonez1@gmail.com>
2021-08-27 12:52:56 -0400

    temporary commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 module.exports = {
-    ENDPOINT: "https://fierce-plains-16077.herokuapp.com/api",
+    ENDPOINT: "http://localhost:8000/api",
+    // ENDPOINT: "https://fierce-plains-16077.herokuapp.com/api",
     REACT_APP_API_KEY: process.env.REACT_APP_API_KEY
 }
