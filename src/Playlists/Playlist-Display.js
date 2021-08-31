a5e65e32e41ba71aa849414c69bc4598d6ec7fe3
nasjones <46979749+nasjones@users.noreply.github.com>
2021-08-31 09:49:45 -0400

    specify node version and updated fetch for playlists

--- src/Playlists/Playlist-Display.js
+++ src/Playlists/Playlist-Display.js
@@ -76,103 +76,74 @@ export default class PlaylistDisplay extends Component {
 			})
 			.then((playlist) => {
 				this.setPlaylist(playlist);
-				//this is the second get request to get the genre information
-				fetch(config.ENDPOINT + `/genres/${playlist.genre_id}`, {
-					method: "GET",
+
+				this.setGenre(playlist);
+				//this rand value is made to offset the songs chosen to keep them random
+				let rand = Math.floor(Math.random() * 400);
+				let queryString =
+					"genre:%20" +
+					playlist.genre_name +
+					"&type=track&limit=50&offset=" +
+					rand;
+
+				let fetData = {
+					qString: queryString,
+				};
+				//this post request is what is made to the spotify api with the genre information
+				fetch(`${config.ENDPOINT}/data`, {
+					method: "POST",
 					headers: {
 						"content-type": "application/json",
 						Authorization: `Bearer ${config.API_KEY}`,
 					},
+					body: JSON.stringify(fetData),
 				})
-					.then((res2) => {
-						if (!res2.ok)
-							return res2.json().then((e) => Promise.reject(e));
-						return res2.json();
+					.then((res3) => {
+						if (!res3.ok)
+							return res3.json().then((e) => Promise.reject(e));
+						return res3.json();
 					})
-					.then((genre) => {
-						this.setGenre(genre);
-						//this rand value is made to offset the songs chosen to keep them random
-						let rand = Math.floor(Math.random() * 1950);
-						let queryString =
-							"genre:%20" +
-							genre.name +
-							"&type=track&limit=50&offset=" +
-							rand;
+					.then((response) => {
+						let runtime = 0;
+						let chosen = [];
+						//this adds songs to the playlist until the runtime criteria is met
+						while (runtime < playlist.length) {
+							let trackChoice = Math.floor(Math.random() * 50);
 
-						let fetData = {
-							qString: queryString,
-						};
-						//this post request is what is made to the spotify api with the genre information
-						fetch(`${config.ENDPOINT}/data`, {
-							method: "POST",
-							headers: {
-								"content-type": "application/json",
-								Authorization: `Bearer ${config.API_KEY}`,
-							},
-							body: JSON.stringify(fetData),
-						})
-							.then((res3) => {
-								if (!res3.ok)
-									return res3
-										.json()
-										.then((e) => Promise.reject(e));
-								return res3.json();
-							})
-							.then((response) => {
-								let runtime = 0;
-								let chosen = [];
-								//this adds songs to the playlist until the runtime criteria is met
-								while (runtime < playlist.length) {
-									let trackChoice = Math.floor(
-										Math.random() * 50
+							if (!chosen.includes(trackChoice)) {
+								chosen.push(trackChoice);
+								let artists = [];
+								for (
+									let i = 0;
+									i <
+									response.tracks.items[trackChoice].artists
+										.length;
+									i++
+								)
+									artists.push(
+										response.tracks.items[trackChoice]
+											.artists[i].name
 									);
 
-									if (!chosen.includes(trackChoice)) {
-										chosen.push(trackChoice);
-										let artists = [];
-										for (
-											let i = 0;
-											i <
-											response.tracks.items[trackChoice]
-												.artists.length;
-											i++
-										)
-											artists.push(
-												response.tracks.items[
-													trackChoice
-												].artists[i].name
-											);
+								let song = {
+									id: response.tracks.items[trackChoice].id,
+									url: response.tracks.items[trackChoice]
+										.external_urls.spotify,
+									title: response.tracks.items[trackChoice]
+										.name,
+									length: response.tracks.items[trackChoice]
+										.duration_ms,
+									explicit:
+										response.tracks.items[trackChoice]
+											.explicit,
+									artist: artists,
+								};
 
-										let song = {
-											id: response.tracks.items[
-												trackChoice
-											].id,
-											url: response.tracks.items[
-												trackChoice
-											].external_urls.spotify,
-											title: response.tracks.items[
-												trackChoice
-											].name,
-											length: response.tracks.items[
-												trackChoice
-											].duration_ms,
-											explicit:
-												response.tracks.items[
-													trackChoice
-												].explicit,
-											artist: artists,
-										};
-
-										runtime += song.length;
-										this.addSongs(song);
-									}
-								}
-								this.setLoaded(true);
-							})
-							.catch((error3) => {
-								console.error({ error3 });
-								this.setErrorTrue();
-							});
+								runtime += song.length;
+								this.addSongs(song);
+							}
+						}
+						this.setLoaded(true);
 					})
 					.catch((error2) => {
 						console.error({ error2 });
--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 module.exports = {
-	ENDPOINT: "https://fierce-plains-16077.herokuapp.com/api",
-	API_KEY: process.env.REACT_API_KEY,
+	ENDPOINT: "http://localhost:8000/api",
+	// "https://fierce-plains-16077.herokuapp.com/api",
+	API_KEY:
+		process.env.REACT_API_KEY || "534879e3-6fdc-4442-8f24-f619396e2724",
 };
