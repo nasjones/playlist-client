757751ebd3318bef7e4683f2f3752d70fc6ef761
nasjones <46979749+nasjones@users.noreply.github.com>
2021-08-31 11:44:18 -0400

    api key name update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
 module.exports = {
 	ENDPOINT: "https://fierce-plains-16077.herokuapp.com/api",
-	API_KEY: process.env.REACT_API_KEY,
+	API_KEY: process.env.REACT_APP_API_KEY,
 };
