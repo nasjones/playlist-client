7d04b768366b074ac8e75ab0502e9983861d1732
nasjones <46979749+nasjones@users.noreply.github.com>
2021-08-31 09:53:04 -0400

    endpoint update

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 module.exports = {
-	ENDPOINT: "http://localhost:8000/api",
-	// "https://fierce-plains-16077.herokuapp.com/api",
-	API_KEY:
-		process.env.REACT_API_KEY || "534879e3-6fdc-4442-8f24-f619396e2724",
+	ENDPOINT: "https://fierce-plains-16077.herokuapp.com/api",
+	API_KEY: process.env.REACT_API_KEY,
 };
